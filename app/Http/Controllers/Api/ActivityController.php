c5ca66e6a5fb46ab0e9811746c7734ee76889c0f
Zilvinas <zilvinas.vilkauskas@gmail.com>
2025-05-23 12:37:56 +0300

    Add index method to ActivityController
    Implemented the index method to return all activities from the Activity model. This provides an API endpoint to retrieve activity data efficiently.

--- app/Http/Controllers/Api/ActivityController.php
+++ app/Http/Controllers/Api/ActivityController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Activity;
 use Illuminate\Http\Request;
 
 class ActivityController extends Controller
 {
-    //
+    public function index()
+    {
+        return Activity::all();
+    }
 }
