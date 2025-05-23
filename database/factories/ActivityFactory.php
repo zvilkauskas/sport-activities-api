502be862dd142a910a4e451a5cc9bbae2bdd54c5
Zilvinas <zilvinas.vilkauskas@gmail.com>
2025-05-23 12:38:02 +0300

    Update ActivityFactory to generate more descriptive names
    Replaced the use of `fake()->name` with `fake()->sentence(3, false)` for the `name` field to improve the descriptiveness of generated activity names. This ensures better data for testing scenarios involving activity names.

--- database/factories/ActivityFactory.php
+++ database/factories/ActivityFactory.php
@@ -20,7 +20,7 @@ class ActivityFactory extends Factory
         return [
             'activity_type' => fake()->randomElement(['Acrobatics', 'Athletics' , 'Pilates', 'Dances']),
             'session_type' => fake()->randomElement(['Group', 'Individual', 'Remote']),
-            'name' => fake()->name,
+            'name' => fake()->sentence(3, false),
             'address' => fake()->streetAddress(),
             'city' => fake()->city(),
             'price' => fake()->randomFloat(2, 10, 200),
