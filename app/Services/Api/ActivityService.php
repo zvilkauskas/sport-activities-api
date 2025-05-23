d6716fef4b987fe038eed1ce89a7d9362c04c9b5
Zilvinas <zilvinas.vilkauskas@gmail.com>
2025-05-23 17:51:01 +0300

    Add activity filtering functionality to API

---
+++ app/Services/Api/ActivityService.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\Api;
+
+use App\Http\Requests\ActivityFilterRequest;
+use App\Models\Activity;
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Collection;
+
+class ActivityService
+{
+    public function getFilteredActivities(ActivityFilterRequest $request): Collection
+    {
+        $query = Activity::query();
+
+        if ($request->has('activity_type')) {
+            $query->where('activity_type', $request->activity_type);
+        }
+
+        if ($request->has('session_type')) {
+            $query->where('session_type', $request->session_type);
+        }
+
+        if ($request->has('city')) {
+            $query->where('city', $request->city);
+        }
+
+        if ($request->has('start_date')) {
+            $query->whereDate('start_date', Carbon::parse($request->start_date));
+        }
+
+        return $query->get();
+    }
+}
